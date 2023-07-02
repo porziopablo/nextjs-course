6c24c223dcc088c393deb5e097967526ee143604
pabloporzio <pabloporzio97@gmail.com>
2023-07-02 19:30:29 -0300

    S3-82 - all events page

--- S4/pages/events/[eventId].tsx
+++ S4/pages/events/[eventId].tsx
@@ -3,7 +3,9 @@ import React from 'react';
 import { useRouter } from 'next/router';
 
 // components
-import EventDetail from '@/content/events/EventDetail';
+import EventSummary from '@/components/events/EventDetail/EventSummary';
+import EventLogistics from '@/components/events/EventDetail/EventLogistics';
+import EventContent from '@/components/events/EventDetail/EventContent';
 
 // repositories
 import { getEventById } from '@/dummy-data';
@@ -17,7 +19,22 @@ function EventDetailPage() {
 
   if (!event) return <p>No event found!</p>;
 
-  return <EventDetail event={event} />;
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        data={{
+          date: event.date,
+          address: event.location,
+          image: event.image,
+          imageAlt: event.title,
+        }}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 }
 
 export default EventDetailPage;
--- S4/pages/events/index.tsx
+++ S4/pages/events/index.tsx
@@ -1,7 +1,20 @@
+// vendors
 import React from 'react';
 
+// components
+import EventList from '@/components/events/EventList/EventList';
+
+// repositories
+import { getAllEvents } from '@/dummy-data';
+
 function EventsPage() {
-  return <div>Events Page</div>;
+  const events = getAllEvents();
+
+  return (
+    <div>
+      <EventList events={events} />
+    </div>
+  );
 }
 
 export default EventsPage;
