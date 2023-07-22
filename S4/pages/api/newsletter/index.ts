93f216b76ab8dadaf00bb06a3701f6d1215c247a
pabloporzio <pabloporzio97@gmail.com>
2023-07-22 18:31:05 -0300

    S9-160 - running mongodb queries

--- S4/pages/api/newsletter/index.ts
+++ S4/pages/api/newsletter/index.ts
@@ -1,25 +1,42 @@
 // vendors
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { MongoClient } from 'mongodb';
 
 // types
 import { NewsletterSubscriptionData } from '@/types/requests/newsletter';
 
-function handlePost(req: NextApiRequest, res: NextApiResponse) {
-  const { email }: NewsletterSubscriptionData = req.body;
+async function handlePost(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { email }: NewsletterSubscriptionData = req.body;
 
-  if (!email?.includes('@')) {
-    return res.status(422).json({ message: 'Invalid email' });
+    if (!email?.includes('@')) {
+      return res.status(422).json({ message: 'Invalid email' });
+    }
+
+    const client = await MongoClient.connect(
+      process.env.NEXT_PUBLIC_MONGODB_URI as string
+    );
+    const db = client.db('newsletter');
+    const collection = db.collection('emails');
+
+    await collection.insertOne({ email });
+
+    await client.close();
+
+    res.status(201).json({ message: 'success' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
   }
-
-  console.log({ email });
-
-  res.status(201).json({ message: 'success' });
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   switch (req.method) {
     case 'POST':
-      handlePost(req, res);
+      await handlePost(req, res);
       break;
     default:
       res.status(404).json({ message: 'Not found' });
